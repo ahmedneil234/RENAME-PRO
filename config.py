3b87d8d50104fbaf0416c9aac9d8e65d83734d82
ahmedneil234 <cinepixserver00@gmail.com>
2025-03-24 14:04:55 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os, re
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8009631616:AAFUwODS5E8pbWkMzkG6XSZ2o346HllGFwA")
+API_ID = int(os.environ.get("API_ID", "26617321"))
+API_HASH = os.environ.get("API_HASH", "40daa567bfe70fb71d9727c4a93b6503")
+OWNER = int(os.environ.get("OWNER", "5488475852"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "@tgupbdbot")
 
 FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031041556"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002506943014"))
 
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://jojix54205:0GtCoMpOVHCjT9sx@mirror.ysg93.mongodb.net/?retryWrites=true&w=majority&appName=mirror")
 DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
 
 STRING = os.environ.get("STRING", "")
